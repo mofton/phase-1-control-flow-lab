ee073e4aaa1702a8b603ce5209035f533027f5a6
Mofeid Moshreky <mof_ton@yahoo.com>
2023-03-16 22:23:36 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  if (distance < 400) {
+    return "This one is on me!";
+  } else if (distance > 400 && distance < 2000) {
+    return "That will be twenty bucks.";
+  } else if (distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distance > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount) {
   // Write your code here!
+  if (tipAmount === "generous") {
+    return "Thank you so much.";
+  } else if (tipAmount === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
